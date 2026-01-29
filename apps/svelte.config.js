723bb3a61e9b780998dc25cb97889f072a48415d
akeit0 <90429982+Akeit0@users.noreply.github.com>
2026-01-29 21:46:49 +0900

    add: github pages

--- apps/svelte.config.js
+++ apps/svelte.config.js
@@ -12,12 +12,15 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
+			pages: process.env.OUT_DIR || 'build',
+			assets: process.env.OUT_DIR || 'build',
+			fallback: '404.html', // Changed to 404.html for better styling on static hosts
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.env.BASE_PATH || ''
+		}
 	}
 };
 
